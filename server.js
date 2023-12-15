449e0630a8fd2d309e712b95e024c7c24068d9c0
Bird-Forest <bird.forest70@gmail.com>
2023-12-15 21:36:53 +0200

    fixed

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Iryna:DYbg8eXGtpLcFnis@cluster0.cgytnih.mongodb.net/books_reader?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
