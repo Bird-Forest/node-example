16d716feb770ae90df01fa257dbc08b5247c609a
Bird-Forest <bird.forest70@gmail.com>
2023-12-15 17:59:45 +0200

    refactoring

--- app.js
+++ app.js
@@ -1,46 +1,35 @@
 const express = require("express");
-const moment = require("moment");
-const fs = require("fs/promises");
+// const moment = require("moment");
+const logger = require("morgan");
+// const fs = require("fs/promises");
 const cors = require("cors");
 
-const books = require("./models/books/books.json");
+// const books = require("./models/books/books.json");
 
-const booksRouter = require("./routes/api/books");
+const booksRouter = require("./routes/api/books"); // імпортуємо маршрути
 
-const app = express();
+const app = express(); // app - веб-сервер
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+app.use(cors()); // перехресні запити та передачі даних між  web-серверами
+app.use(express.json()); // для обробки формату запитів та відповіді - веб-сервер, frontend
+app.use("/api/books", booksRouter); // обробляє маршрути вказуємо endpoint - /api/books
+app.use(logger(formatsLogger));
 
-// const corsMiddleware = cors();
-// app.use(corsMiddleware);
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/books", booksRouter);
-
-// app.use(async (req, res, next) => {
-//   const { method, url } = req;
-//   const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-//   await fs.appendFile("./pablic/server.log", `\n${method} ${url} ${date}`);
-//   next();
+// app.get("/", (request, response) => {
+//   // запит - веб-сервер
+//   // response.send("<h1>Home page</h1>"); не вміє обробляти
+//   response.json("<h1>Home page</h1>");
 // });
-
-// app.use((req, res, next) => {
-//   console.log("Next");
-//   next();
-// });
-
-app.get("/", (request, response) => {
-  response.send("<h1>Home page</h1>");
-});
 
 // app.get("/contacts", (request, response) => {
 //   console.log(request.url);
 //   console.log(request.method);
 //   response.send("<h1>Contacts page</h1>");
 // });
-app.get("/products", (request, response) => {
-  // response.send(books);
-  response.json([]);
-});
+// app.get("/products", (request, response) => {
+//   // response.send(books);
+//   response.json([]);
+// });
 
 // app.get("/books", (request, response) => {
 //   const databaseResponse = null;
@@ -51,12 +40,16 @@ app.get("/products", (request, response) => {
 // });
 
 app.use((req, res) => {
+  // функція спрацьовує коли відсутня сторінка
   res.status(404).json({ message: "Not found" });
 });
 
+// Загальна помилка серверу
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => console.log("Server running"));
+// app.listen(3000, () => console.log("Server running")); - перенесли в файл server
+
+module.exports = app;
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,84 +1,23 @@
 const express = require("express");
-const Joi = require("joi");
 
-const books = require("../../models/books");
-const { HttpError } = require("../../helpers");
-
-// const app = express();
+// файл з якого експортуються маршрути
 const router = express.Router();
 
-const addSchema = Joi.object({
-  title: Joi.string().required(),
-  author: Joi.string().required(),
-});
+const ctrl = require("../../controllers/books");
 
-router.get("/", async (req, res, next) => {
-  // res.json(books);
-  try {
-    const result = await books.getAll();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+const { validateBody } = require("../../middlewares");
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await books.getById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+const schema = require("../../schemas/books");
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    console.log(error);
-    if (error) {
-      throw HttpError(400, error);
-    }
-    const result = await books.add(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", ctrl.getAll);
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    console.log(error);
-    if (error) {
-      throw HttpError(400, error);
-    }
-    const { id } = req.params;
-    const result = await books.updateById(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrl.getById);
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await books.deleteById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({ result, message: "Delete success" });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validateBody(schema), ctrl.add);
+
+router.put("/:id", validateBody(schema), ctrl.updateById);
+
+router.delete("/:id", ctrl.deleteById);
 
 module.exports = router;
 
